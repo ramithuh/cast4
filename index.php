1ea067bc0863f3060b9285d7e15eef4d17621f5f
Ramith Hettiarachchi <ramithuh@live.com>
2019-06-07 22:57:50 +0530

    no message

--- index.php
+++ index.php
@@ -202,7 +202,6 @@ It's time for Cast4 for the year 2019...
 
 <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Frotaractmora%2Fphotos%2Fa.2135810299801353%2F2135819486467101%2F%3Ftype%3D3&width=500" width="500" height="337" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 
-<center><img src="IMG_0383.JPG" width="400" height="400"></center>
 						
 </div>
 						
